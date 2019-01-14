d31d10b6eea3ae0de8ad9354a3f316214f1289c0
Aaron Breault <aaronbreault@gmail.com>
2019-01-14 11:39:55 -0800

    Renamed files to something more permanent and professional.

--- testharness.pl
+++ testharness.pl
@@ -2,7 +2,7 @@
 use warnings;
 use strict;
 
-my $testcases = 'testcases.txt';
+my $testcases = 'testtestcases.txt';
 open (my $in, "<:encoding(utf8)", $testcases) or die "$testcases: $!";
 my @lines = <$in>;
 close $in;
@@ -11,7 +11,6 @@ my $stdout;;
 chomp @lines;
 for my $line (@lines) {
     #print "theline: $line\n";
-    #Run bech32.pl with arguments in $line and return the standard out to $stdout.
     $stdout = qx/perl bech32.pl $line/; 
     print "stdout:$stdout\n";
 
